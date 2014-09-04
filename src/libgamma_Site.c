1f8f46a7143566ea21f871719db024dc7a77ba89
Mattias Andrée <maandree@operamail.com>
2014-09-05 01:48:30 +0200

    implamant c code for site
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/libgamma_Site.c
+++ src/libgamma_Site.c
@@ -17,11 +17,54 @@
  */
 #include "libgamma_Site.h"
 
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
 #include <libgamma.h>
 
 
 #define J  JNIEnv* env, jclass class
 
+
+
+/**
+ * Make a failure-return.
+ * 
+ * @param   error_code  The error code returned from the failing function or zero to read `errno`.
+ * @return              The object to return.
+ */
+static jlongArray fail(JNIEnv* env, int error_code)
+{
+  jlongArray rc = (*env)->NewLongArray(env, 3);
+  jlong e, z = 0;
+  if ((error_code == LIBGAMMA_ERRNO_SET) || !error_code)
+    error_code = errno;
+  e = (jlong)error_code;
+  (*env)->SetLongArrayRegion(env, rc, 0, 1, &z);
+  (*env)->SetLongArrayRegion(env, rc, 1, 1, &z);
+  (*env)->SetLongArrayRegion(env, rc, 2, 1, &e);
+  return rc;
+}
+
+
+/**
+ * Make a success-return.
+ * 
+ * @param   state  The native object.
+ * @param   count  The subelement count.
+ * @return         The object to return.
+ */
+static jlongArray ok(JNIEnv* env, void* state, size_t count)
+{
+  jlong a = (jlong)(size_t)state;
+  jlong b = (jlong)count, z = 0;
+  jlongArray rc = (*env)->NewLongArray(env, 3);
+  (*env)->SetLongArrayRegion(env, rc, 0, 1, &a);
+  (*env)->SetLongArrayRegion(env, rc, 1, 1, &b);
+  (*env)->SetLongArrayRegion(env, rc, 2, 1, &z);
+  return rc;
+}
 
 
 /**
@@ -33,7 +76,37 @@
  *                  Element 1:  The value for {@link #partitions_available}
  *                  Element 2:  Error code, zero on success.
  */
-jlongArray Java_libgamma_Site_libgamma_1site_1create(J, jint method, jstring site);
+jlongArray Java_libgamma_Site_libgamma_1site_1create(J, jint method, jstring site)
+{
+  libgamma_site_state_t* state = malloc(sizeof(libgamma_site_state_t));
+  const char* site_chars;
+  char* site_;
+  int r;
+  if (site != NULL)
+    {
+      int saved_errno = 0;
+      size_t n;
+      site_chars = (*env)->GetStringUTFChars(env, site, NULL);
+      n = strlen(site_chars) + 1;
+      site_ = malloc(n * sizeof(char));
+      if (site_ == NULL)
+	saved_errno = errno;
+      else
+	memcpy(site_, site_chars, n * sizeof(char));
+      (*env)->ReleaseStringUTFChars(env, site, site_chars);
+      if (saved_errno)
+	return fail(env, errno);
+    }
+  else
+    site_ = NULL;
+  if (state == NULL)
+    return fail(env, 0);
+  r = libgamma_site_initialise(state, method, site_);
+  if (r != 0)
+    return fail(env, r);
+  return ok(env, state, state->partitions_available);
+  (void) class;
+}
 
 
 /**
@@ -42,7 +115,13 @@ jlongArray Java_libgamma_Site_libgamma_1site_1create(J, jint method, jstring sit
  * 
  * @param  address  The site state.
  */
-void Java_libgamma_Site_libgamma_1site_1free(J, jlong address);
+void Java_libgamma_Site_libgamma_1site_1free(J, jlong address)
+{
+  void* this = (void*)(size_t)address;
+  libgamma_site_free(this);
+  (void) env;
+  (void) class;
+}
 
 
 /**
@@ -51,5 +130,14 @@ void Java_libgamma_Site_libgamma_1site_1free(J, jlong address);
  * @param   address  The site state.
  * @return           Zero on success, and error code on failure.
  */
-jint Java_libgamma_Site_libgamma_1site_1restore(J, jlong address);
+jint Java_libgamma_Site_libgamma_1site_1restore(J, jlong address)
+{
+  void* this = (void*)(size_t)address;
+  int r = libgamma_site_restore(this);
+  if (r != 0)
+    return r == LIBGAMMA_ERRNO_SET ? errno : r;
+  return 0;
+  (void) env;
+  (void) class;
+}
 
